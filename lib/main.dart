f990d78e72ee4bfeb508e957832a8d795294f820
am-anshuman <anshumansahu0310@gmail.com>
2023-06-23 20:44:27 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:expense_tracker/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
